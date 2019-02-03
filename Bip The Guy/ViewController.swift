1bd3d26d8ffb9f4298ee8cad504b53abcb5d9a9e
Jason Tee <Jason@macbook-pro.local>
2019-02-02 20:16:30 -0500

    Initial Commit

---
+++ Bip The Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip The Guy
+//
+//  Created by Jason Tee on 2/2/19.
+//  Copyright © 2019 Jason Tee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
